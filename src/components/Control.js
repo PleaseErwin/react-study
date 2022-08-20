a4543db6935c53c93118ecf7deb4d3389d7d8aec
PleaseErwin <sowon6257@gmail.com>
2022-08-20 23:55:24 +0900

    cancel 버튼 추가 및 간격 생성

--- src/components/Control.js
+++ src/components/Control.js
@@ -3,20 +3,24 @@ import { Component } from 'react';
 class Control extends Component {
   render(){
     return (
-      <dir>
-        <a href="/create" onClick={function(e){
-            e.preventDefault();
-            this.props.onChangeMode('create');
-          }.bind(this)}>create </a> 
-        <a href="/update" onClick={function(e){
-            e.preventDefault();
-            this.props.onChangeMode('update');
-          }.bind(this)}>update </a> 
+      <div>
+        <input type="button" value="create" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('create');
+          }.bind(this)}></input>
+        <input type="button" value="update" onClick={function(e){
+          e.preventDefault();
+          this.props.onChangeMode('update');
+          }.bind(this)}></input>
         <input type="button" value="delete" onClick={function(e){
             e.preventDefault();
             this.props.onChangeMode('delete');
           }.bind(this)}></input>
-      </dir>
+        <input type="button" value="cancel" onClick={function(e){
+            e.preventDefault();
+            this.props.onChangeMode('read');
+          }.bind(this)}></input>
+      </div>
     );
   }
 }
